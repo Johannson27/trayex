775e35a2cc03a775614d9d6e87cff0fa4aa210f9
Johannson27 <johanquintanilla74@gmail.com>
2025-11-07 20:35:53 -0600

    cambios en el manifest

--- trayex-front/app/layout.tsx
+++ trayex-front/app/layout.tsx
@@ -6,12 +6,21 @@ import "./globals.css";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "TRAYEX",
   description: "Trayectoria y Exactitud",
   manifest: "/manifest.json",
   themeColor: "#204284",
+  icons: {
+    icon: [
+      { url: "/icons/icon-192.png", sizes: "192x192", type: "image/png" },
+      { url: "/icons/icon-512.png", sizes: "512x512", type: "image/png" },
+    ],
+    apple: [{ url: "/icons/icon-192.png" }],
+  },
 };
+
+
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (

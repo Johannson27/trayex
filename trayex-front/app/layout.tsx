535859a88fd7e603f7827123970d78346ed4f2cd
Johannson27 <johanquintanilla74@gmail.com>
2025-11-10 05:03:33 -0600

    cambios listos para subir a render

--- trayex-front/app/layout.tsx
+++ trayex-front/app/layout.tsx
@@ -3,6 +3,7 @@ import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "TRAYEX",
@@ -23,17 +24,9 @@ export const viewport: Viewport = {
   themeColor: "#204284"
 };
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const mapsKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
   return (
     <html lang="es">
-      <head>
-        {mapsKey && (
-          <script async
-            src={`https://maps.googleapis.com/maps/api/js?key=${mapsKey}&libraries=places`}>
-          </script>
-        )}
-      </head>
-      <body>{children}</body>
+      <body className="font-sans antialiased">{children}</body>
     </html>
   );
 }
